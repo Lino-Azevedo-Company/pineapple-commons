db0adb63a4bb814501584632f143b88c2ae3b1a0
TobiasLino <linop019@gmail.com>
2022-08-05 13:37:41 -0300

    Add: Common set of application exceptions

---
+++ src/main/java/com/pineapple/commons/exception/JwtTokenException.java
@@ -0,0 +1,12 @@
+package com.pineapple.commons.exception;
+
+public class JwtTokenException extends RuntimeException {
+
+    public JwtTokenException(String message) {
+        super(message);
+    }
+
+    public JwtTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
